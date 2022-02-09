ad09991928b8d867c76e937d26da8821e94dfcbc
ricardo <ricardojara01@gmail.com>
2022-02-09 19:25:01 +0100

    Added README.MD

---
+++ src/test/java/test/procesadoCsv/ProcesarFicheroCalculosTotalesTest.java
@@ -0,0 +1,12 @@
+package test.procesadoCsv;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class ProcesarFicheroCalculosTotalesTest {
+	@Order(1)
+    @Test
+    public void testFicheroEjemplo_correcto_Julio2021() {
+		
+	}
+}
